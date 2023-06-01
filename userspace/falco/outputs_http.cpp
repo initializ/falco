edc88d914a9f478b6f9d4da76b610142cf1a79fd
Federico Di Pierro <nierro92@gmail.com>
2023-06-01 11:19:01 +0200

    cleanup(userspace/falco): improvements to the http output perf.
    Moreover, add option to disable stdout echoing.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/falco/outputs_http.cpp
+++ userspace/falco/outputs_http.cpp
@@ -18,23 +18,32 @@ limitations under the License.
 #include "logger.h"
 #include "banned.h" // This raises a compilation error when certain functions are used
 
-void falco::outputs::output_http::output(const message *msg)
+static size_t noop_write_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-	CURL *curl = NULL;
-	CURLcode res = CURLE_FAILED_INIT;
-	struct curl_slist *slist1;
-	slist1 = NULL;
+	// We don't want to echo anything. Just return size of bytes ignored
+	return size * nmemb;
+}
 
-	curl = curl_easy_init();
-	if(curl)
+bool falco::outputs::output_http::init(const config& oc, bool buffered, const std::string& hostname, bool json_output, std::string &err)
+{
+	falco::outputs::abstract_output::init(oc, buffered, hostname, json_output, err);
+
+	m_curl = nullptr;
+	m_http_headers = nullptr;
+	CURLcode res = CURLE_FAILED_INIT;
+
+	m_curl = curl_easy_init();
+	if(m_curl)
 	{
-		if (m_json_output)
+		if(m_json_output)
 		{
-			slist1 = curl_slist_append(slist1, "Content-Type: application/json");
-		} else {
-			slist1 = curl_slist_append(slist1, "Content-Type: text/plain");
+			m_http_headers = curl_slist_append(m_http_headers, "Content-Type: application/json");
 		}
-		res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist1);
+		else
+		{
+			m_http_headers = curl_slist_append(m_http_headers, "Content-Type: text/plain");
+		}
+		res = curl_easy_setopt(m_curl, CURLOPT_HTTPHEADER, m_http_headers);
 
 		if(res == CURLE_OK)
 		{
@@ -47,62 +56,83 @@ void falco::outputs::output_http::output(const message *msg)
 			{
 				unquotedUrl = libsinsp::filter::unescape_str(unquotedUrl);
 			}
-			res = curl_easy_setopt(curl, CURLOPT_URL, unquotedUrl.c_str());
+			res = curl_easy_setopt(m_curl, CURLOPT_URL, unquotedUrl.c_str());
 		}
 
 		if(res == CURLE_OK)
 		{
-			res = curl_easy_setopt(curl, CURLOPT_POSTFIELDS, msg->msg.c_str());
+			res = curl_easy_setopt(m_curl, CURLOPT_USERAGENT, m_oc.options["user_agent"].c_str());
 		}
 
 		if(res == CURLE_OK)
 		{
-			res = curl_easy_setopt(curl, CURLOPT_USERAGENT, m_oc.options["user_agent"].c_str());
-		}
-
-		if(res == CURLE_OK)
-		{
-		   res = curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1L);
+			res = curl_easy_setopt(m_curl, CURLOPT_POSTFIELDSIZE, -1L);
 		}
 
 		if(res == CURLE_OK)
 		{
 			if(m_oc.options["insecure"] == std::string("true"))
 			{
-				res = curl_easy_setopt(curl,CURLOPT_SSL_VERIFYPEER, 0L);
+				res = curl_easy_setopt(m_curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
 				if(res == CURLE_OK)
 				{
-					res = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+					res = curl_easy_setopt(m_curl, CURLOPT_SSL_VERIFYHOST, 0L);
 				}
 			}
 		}
 
 		if(res == CURLE_OK)
 		{
-			if (!m_oc.options["ca_cert"].empty())
+			if(!m_oc.options["ca_cert"].empty())
 			{
-				res = curl_easy_setopt(curl, CURLOPT_CAINFO, m_oc.options["ca_cert"].c_str());
-			}else if(!m_oc.options["ca_bundle"].empty())
+				res = curl_easy_setopt(m_curl, CURLOPT_CAINFO, m_oc.options["ca_cert"].c_str());
+			}
+			else if(!m_oc.options["ca_bundle"].empty())
 			{
-				res = curl_easy_setopt(curl, CURLOPT_CAINFO, m_oc.options["ca_bundle"].c_str());
-			}else{
-				res = curl_easy_setopt(curl, CURLOPT_CAPATH, m_oc.options["ca_path"].c_str());
+				res = curl_easy_setopt(m_curl, CURLOPT_CAINFO, m_oc.options["ca_bundle"].c_str());
+			}
+			else
+			{
+				res = curl_easy_setopt(m_curl, CURLOPT_CAPATH, m_oc.options["ca_path"].c_str());
 			}
 		}
 
-  		if(res == CURLE_OK)
+		if(m_oc.options["echo"] == std::string("false"))
 		{
-			res = curl_easy_perform(curl);
+			// If unset, libcurl defaults to fwrite to stdout, ie: echoing
+			if(res == CURLE_OK)
+			{
+				res = curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, noop_write_callback);
+			}
 		}
-
-		if(res != CURLE_OK)
-		{
-			falco_logger::log(LOG_ERR, "libcurl error: " + std::string(curl_easy_strerror(res)));
-		}
-		curl_easy_cleanup(curl);
-		curl = NULL;
-		curl_slist_free_all(slist1);
-		slist1 = NULL;
 	}
+
+	if(res != CURLE_OK)
+	{
+		err = "libcurl error: " + std::string(curl_easy_strerror(res));
+		return false;
+	}
+	return true;
+}
+
+void falco::outputs::output_http::output(const message *msg)
+{
+	CURLcode res = curl_easy_setopt(m_curl, CURLOPT_POSTFIELDS, msg->msg.c_str());
+	if (res == CURLE_OK)
+	{
+		res = curl_easy_perform(m_curl);
+	}
+	if(res != CURLE_OK)
+	{
+		falco_logger::log(LOG_ERR, "libcurl failed to perform call: " + std::string(curl_easy_strerror(res)));
+	}
+}
+
+void falco::outputs::output_http::cleanup()
+{
+	curl_easy_cleanup(m_curl);
+	m_curl = nullptr;
+	curl_slist_free_all(m_http_headers);
+	m_http_headers = nullptr;
 }
